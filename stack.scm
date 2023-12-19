6eea7b190d9ee4a2c3ebf9b41fafa62d92526e8a
Birame S. <birame.sene@zenika.com>
2023-12-19 23:25:34 +0100

    implements null stack

--- stack.scm
+++ stack.scm
@@ -1,7 +1,16 @@
 (define (empty stack) (= 0 (length stack)))
+(define (size stack) 0)
 
 (define (create-stack) '())
 
-(define (push value stack) (cons value stack))
+(define (push value stack) 
+  (if (null? stack) 
+    (error "stackoverflow!")
+    (cons value stack)))
 
-(define (pop stack) '())
+(define (pop stack) 
+  (if (null? stack)
+    (error "stackunderflow!")
+    '()))
+
+(define (peek stack) (error "stackunderflow!"))
--- stack_test.scm
+++ stack_test.scm
@@ -2,7 +2,25 @@
 
 (load "stack.scm")
 
-(test "new created stack is empty" #t (empty (create-stack)))
-(test "stack is NOT empty when push value on it" #f (empty (push "D" (create-stack) )))
+(handle-exceptions exn
+                   (lambda ()
+                           (display "Caught an error: ")
+                           (display (condition-message exn))
+                           (newline))
+                   (error "This is an error"))
 
-(test "stack is empty when pop push and pop value" #t (empty (pop (push "D" (create-stack) ))))
+(test-begin "stack specifications")
+
+(test-group "Null stack operations"
+            (test "null stack is empty" #t (empty (create-stack)))
+            (test "null stack has size of 0" 0 (size (create-stack)))
+            (test-error "prevent from push on null stack" ((lambda () (push 9 (create-stack)))))
+            (test-error "prevent from pop on null stack"  ((lambda () (pop (create-stack)))))
+            (test-error "prevent from peek on null stack" ((lambda ()  (peek (create-stack)))))
+            
+            )
+;(test "stack is NOT empty when push value on it" #f (empty (push "D" (create-stack) )))
+
+;(test "stack is empty when pop push and pop value" #t (empty (pop (push "D" (create-stack) ))))
+
+(test-end "stack specifications")
